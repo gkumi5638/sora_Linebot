7aa288f40e33b869a6fd131815af94c6719d8fd3
kumi <>
2018-12-01 18:02:41 +0900

    マイグレーションファイルにline_idとnull:falseを追記

--- db/migrate/20181201082000_create_users.rb
+++ db/migrate/20181201082000_create_users.rb
@@ -1,7 +1,7 @@
 class CreateUsers < ActiveRecord::Migration[5.2]
   def change
     create_table :users do |t|
-      t.string :line_id
+      t.string :line_id, null: false  #指定したカラムが空（NULL）の状態でテーブル内に保存されることを許可しない
 
       t.timestamps
     end
